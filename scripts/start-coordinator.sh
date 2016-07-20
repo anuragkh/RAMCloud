8fe980cf694bf9be4786358dc899b88e3fcaf544
Anurag Khandelwal <anuragk@berkeley.edu>
2016-07-20 16:43:14 +0000

    Updated start server/coordinator scripts

--- scripts/start-coordinator.sh
+++ scripts/start-coordinator.sh
@@ -2,6 +2,7 @@ sbin="`dirname "$0"`"
 sbin="`cd "$sbin"; pwd`"
 
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$sbin/../obj.master"
+ip=`wget -qO- http://instance-data/latest/meta-data/public-ipv4`
 
 rm -f coordinator.log
-nohup $sbin/../obj.master/coordinator -C tcp:host=0.0.0.0,port=11211 --logFile coordinator.log >/dev/null 2>/dev/null &
+nohup $sbin/../obj.master/coordinator -C tcp:host=${ip},port=11211 --logFile coordinator.log >/dev/null 2>/dev/null &
