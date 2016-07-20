557457acc699c112a9a98c6524567e757421fbd9
Anurag Khandelwal <anuragk@berkeley.edu>
2016-07-20 02:15:42 +0000

    Updated scripts

--- scripts/start-coordinator.sh
+++ scripts/start-coordinator.sh
@@ -5,4 +5,3 @@ export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$sbin/../obj.master"
 
 rm -f coordinator.log
 nohup $sbin/../obj.master/coordinator -C tcp:host=0.0.0.0,port=11211 --logFile coordinator.log >/dev/null 2>/dev/null &
-
