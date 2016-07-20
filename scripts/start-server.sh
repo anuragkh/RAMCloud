8e0f52a7ec41bc2095606d881a5449ad1ea57456
Anurag Khandelwal <anuragk@berkeley.edu>
2016-07-20 16:48:40 +0000

    Fixed scripts

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -2,7 +2,7 @@ sbin="`dirname "$0"`"
 sbin="`cd "$sbin"; pwd`"
 
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$sbin/../obj.master"
-ip=`wget -qO- http://instance-data/latest/meta-data/public-ipv4`
+ip=`hostname`
 
 rm -f server.log
 nohup $sbin/../obj.master/server -C tcp:host=${ip},port=11211 -L tcp:host=${ip},port=11101 -r 0 --allowLocalBackup --maxNonVolatileBuffers=0 --totalMasterMemory=20000 --logFile server.log >/dev/null 2>/dev/null &
